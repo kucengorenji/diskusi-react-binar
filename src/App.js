8479501474ac772419d42d98a54021c5b99d5e18
mikelrisyad <mikelrisyad@gmail.com>
2021-12-02 01:15:06 +0700

    add function component example with prop

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import './App.css';
 import ClassComponent from './components/1-react-component-basic/ClassComponent';
 import FunctionComponent from './components/1-react-component-basic/FunctionComponent';
 import PropsExample from './components/2-props-vs-state/PropsExample';
+import PropsExampleFunction from './components/2-props-vs-state/PropsExampleFunction';
 import StateExample from './components/2-props-vs-state/StateExample';
-import HooksExample from './components/3-hooks/HooksExample'
-import ModuleCss from './components/4-styling-component/ModuleCss'
-import Counter from './components/5-contoh-lain/Counter'
+import HooksExample from './components/3-hooks/HooksExample';
+import ModuleCss from './components/4-styling-component/ModuleCss';
+import Counter from './components/5-contoh-lain/Counter';
 
 function App() {
   return (
     <div className="App">
       <ClassComponent text="world, ini class component" />
       <FunctionComponent text="World!, ini function component" />
-      <PropsExample text="contoh tambahan props example"/>
+      <PropsExample text="contoh tambahan props example" />
+      <PropsExampleFunction text="ini text tambahan di function component" />
       <StateExample />
       <HooksExample />
       <ModuleCss />
---
+++ src/components/2-props-vs-state/PropsExampleFunction.jsx
@@ -0,0 +1,7 @@
+export default function PropsExampleFunction({text}) {
+    return (
+        <>
+            <div>Hello World, ini props example pake function component, {text}</div>
+        </>
+    )
+};
