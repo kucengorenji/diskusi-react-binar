cdeeeecefc97e5e5e5be17cdf261e043232c8a73
mikelrisyad <mikelrisyad@gmail.com>
2021-12-02 01:11:27 +0700

    add inline css example

--- src/components/4-styling-component/ModuleCss.jsx
+++ src/components/4-styling-component/ModuleCss.jsx
@@ -2,12 +2,20 @@ import styles from './style.module.css'; // Import css modules stylesheet as sty
 import './style.css'; // Import regular stylesheet
 
 const ModuleCss = () => {
-    return (
-        <>
-            <h1 className={styles.textColor}>Welcome to the hell brother</h1>
-            <h1 class="text-biru">ini yang pakai stylesheet css biasa</h1>
-        </>
-    )
-}
+  const gaya = {
+    color: 'red',
+    fontSize: 45,
+  };
+
+  return (
+    <>
+      <h1 className={styles.textColor}>Welcome to the hell brother</h1>
+      <h1 class="text-biru">ini yang pakai stylesheet css biasa</h1>
+      <h1>
+        ini yang pakai inline <span style={gaya}>css dengan gaya</span>
+      </h1>
+    </>
+  );
+};
 
 export default ModuleCss;
